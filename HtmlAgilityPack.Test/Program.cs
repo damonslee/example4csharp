bcf1e1e994cafb4a52fd83c21eaa2fe8ed1a436b
damon <damon@nanoomi.co.kr>
2020-02-06 15:30:37 +0900

    add htmlagilitypackage

--- HtmlAgilityPack.Test/Program.cs
+++ HtmlAgilityPack.Test/Program.cs
@@ -9,7 +9,7 @@ namespace HtmlAgilityPack.Test
         {
             string docurl   = "https://ebiz.khnp.co.kr/getTmpView.do?type=spbidnoti&loadId=fff84246-b957-47fc-a249-e1d023131890";
             var doc         = new HtmlWeb().Load(docurl).DocumentNode;
-		    string aprice   = doc.SelectSingleNode("//th[text(),'가격점수제외금액(A)']").NextSibling.NextSibling.InnerText;
+		    string aprice   = doc.SelectSingleNode("//th[text()='가격점수제외금액(A)']").NextSibling.NextSibling.InnerText;
 
             Console.WriteLine(aprice);
             
